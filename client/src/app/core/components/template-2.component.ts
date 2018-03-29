7349639a5ff02091bcd2a213d0631a33d9977761
Sam Zagrobelny <strangesast@gmail.com>
2018-03-29 12:42:56 -0400

    first svg drag

--- client/src/app/core/components/template-2.component.ts
+++ client/src/app/core/components/template-2.component.ts
@@ -2,6 +2,7 @@ import { AfterViewInit, Component, ElementRef, QueryList, ViewChild, ViewChildre
 import { FormArray, FormBuilder, FormControl, Validators } from '@angular/forms';
 import * as uuid from 'uuid/v4';
 import * as d3 from 'd3';
+import { Selection, ZoomBehavior } from 'd3';
 
 // import 'rxjs/add/operator/startWith';
 import { from } from 'rxjs/observable/from';
@@ -39,28 +40,42 @@ enum GridTypes {
     </div>
     <div formArrayName="elements">
       <div *ngFor="let element of elements.controls; let i=index; trackBy: element?.value._id" [formGroupName]="i">
-        <label>Element {{ i + 1 }}</label>
-        <mat-form-field>
-          <input matInput type="text" placeholder="text" formControlName="text">
-        </mat-form-field>
-        <input formControlName="color" type="color">
-        <mat-form-field>
-          <mat-select placeholder="Font" formControlName="font">
-            <mat-option *ngFor="let font of fonts" [attr.font-family]="font" [value]="font">
-              {{ font }}
-            </mat-option>
-          </mat-select>
-        </mat-form-field>
-        <button type="button" mat-button (click)="removeElement(i)">Remove</button>
+        <div>
+          <label>Element {{ i + 1 }}</label>
+          <mat-form-field>
+            <input matInput type="text" placeholder="text" formControlName="text">
+          </mat-form-field>
+        </div>
+        <div>
+          <label>Color</label>
+          <input formControlName="color" type="color">
+          <mat-form-field>
+            <mat-select placeholder="Font" formControlName="font">
+              <mat-option *ngFor="let font of fonts" [attr.font-family]="font" [value]="font">
+                {{ font }}
+              </mat-option>
+            </mat-select>
+          </mat-form-field>
+        </div>
+        <div>
+          <mat-form-field>
+            <input matInput type="number" placeholder="x" step="1" formControlName="x">
+          </mat-form-field>
+          <mat-form-field>
+            <input matInput type="number" placeholder="y" step="1" formControlName="y">
+          </mat-form-field>
+        </div>
+        <div>
+          <button type="button" mat-button (click)="removeElement(i)">Remove</button>
+        </div>
       </div>
     </div>
     <button type="button" mat-raised-button (click)="addElement()">Add Element</button>
   </form>
+  <button mat-raised-button type="button" (click)="resetTransform()">Reset Transform</button>
   <svg #svgElement xmlns="http://www.w3.org/2000/svg"
     *ngIf="{ width: width$ | async, height: height$ | async } as size"
-    [attr.viewBox]="[0, 0, size.width, size.height].join(' ')"
-    [attr.width]="size.width * 10"
-    [attr.height]="size.height * 10">
+    [attr.viewBox]="[0, 0, size.width, size.height].join(' ')">
     <defs>
       <!-- grid -->
       <pattern id="smallGrid" width="1" height="1" patternUnits="userSpaceOnUse">
@@ -80,33 +95,39 @@ enum GridTypes {
         <rect fill="url(#dots)" width="100%" height="100%"/>
       </mask>
     </defs>
-    <rect width="100%" height="100%" [attr.fill]="background.value"/>
-    <g *ngFor="let element of elements.controls; trackBy: element?.value._id" #els>
-      <text
-        x="0"
-        y="0"
-        [attr.font-family]="element.value.font"
-        [attr.font-size]="fontSizes[element.value.font].size"
-        text-anchor="start"
-        [attr.fill]="element.value.color"
-        mask="url(#pattern-mask)"
-        alignment-baseline="hanging">
-        {{element.value.text}}
-      </text>
+    <g>
+      <text x="0" y="-1" font-size="10" fill="#999">Board 1</text>
+      <rect [attr.width]="size.width" [attr.height]="size.height" [attr.fill]="background.value"/>
+      <g
+        #els
+        *ngFor="let element of elements.controls; trackBy: element?.value._id"
+        [attr.id]="element.value._id"
+        [attr.transform]="'translate(' + (element.value.x || 0) + ',' + (element.value.y || 0) + ')'">
+        <text
+          [attr.transform]="fontSizes[element.value.font].adj"
+          [attr.font-family]="element.value.font"
+          [attr.font-size]="fontSizes[element.value.font].size"
+          text-anchor="start"
+          [attr.fill]="element.value.color"
+          mask="url(#pattern-mask)"
+          alignment-baseline="hanging">
+          {{element.value.text}}
+        </text>
+      </g>
+      <rect
+        *ngIf="gridType.value == gridTypes.Dots"
+        class="noselect"
+        [attr.width]="size.width"
+        [attr.height]="size.height"
+        [attr.fill]="background.value"
+        mask="url(#circles-mask)"/>
+      <rect
+        *ngIf="gridType.value == gridTypes.Grid"
+        class="noselect"
+        [attr.width]="size.width"
+        [attr.height]="size.height"
+        fill="url(#grid)"/>
     </g>
-    <rect
-      *ngIf="gridType.value == gridTypes.Dots"
-      class="noselect"
-      width="100%"
-      height="100%"
-      [attr.fill]="background.value"
-      mask="url(#circles-mask)"/>
-    <rect
-      *ngIf="gridType.value == gridTypes.Grid"
-      class="noselect"
-      width="100%"
-      height="100%"
-      fill="url(#grid)"/>
   </svg>
     `,
   styles: [
@@ -129,6 +150,8 @@ enum GridTypes {
 })
 export class Template2Component implements AfterViewInit {
   @ViewChild('svgElement') svg: ElementRef;
+  svgSelection: Selection<SVGElement, {}, HTMLElement, any>;
+  zoom: ZoomBehavior<Element, {}>;
 
   @ViewChildren('els') els: QueryList<any>;
 
@@ -136,7 +159,14 @@ export class Template2Component implements AfterViewInit {
   gridTypeValues = Object.keys(GridTypes).filter(k => !isNaN(Number(GridTypes[k])));
 
   fontSizes = {
-    'font9x18': { size: 18 },
+    'font9x18': { size: 18, adj: `translate(${ 0.01 - 1 },${ 0.21 - 1 })` },
+    'font6x12': { size: 12, adj: `translate(${ 0 },${ -1.35 })` },
+    'font10x20': { size: 20, adj: `translate(${ -1 },${ 0.2 })` },
+    'font6x13O': { size: 13, adj: `translate(${ 0 },${ 0.4 }) scale(1.0015)` },
+    'font4x6': { size: 6, adj: `translate(${ 0 },${ 0.8 }) scale(1.001)` },
+    'font5x7': { size: 7, adj: `translate(${ 0 },${ 0.8 }) scale(1.0005)` },
+    'font5x8': { size: 8, adj: `translate(${ 0 },${ 0.60 })` },
+    'font6x10': { size: 10, adj: `translate(${ 0 },${ 0.6 })` },
   };
   fonts = Object.keys(this.fontSizes);
 
@@ -145,16 +175,9 @@ export class Template2Component implements AfterViewInit {
       width: [128, Validators.required],
       height: [32, Validators.required],
     }),
-    gridType: [this.gridTypes.None],
+    gridType: [this.gridTypes.Grid],
     background: ['#000000'],
-    elements: this.fb.array([
-      this.fb.group({
-        _id: uuid(),
-        text: ['Text', Validators.required],
-        color: ['#FFFFFF'],
-        font: ['font9x18'],
-      })
-    ]),
+    elements: this.fb.array([]),
   });
 
   get gridType() {
@@ -183,36 +206,55 @@ export class Template2Component implements AfterViewInit {
     distinctUntilChanged(),
   );
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder) {
+    this.addElement();
+  }
+
+  resetTransform() {
+    this.svgSelection.call(this.zoom.transform, d3.zoomIdentity);
+  }
 
   ngAfterViewInit() {
-    const svg = this.svg.nativeElement;
+    const elements = this.elements;
+
+    const svgSelection = d3.select(this.svg.nativeElement);
+    this.svgSelection = svgSelection;
+    const svg = this.svgSelection.select('g');
+
+    const pt = (this.svgSelection.node() as any).createSVGPoint();
 
     let pos = [0, 0];
     function dragstarted(d) {
-      const s = d3.select(this);
-      const bbox = this.getBoundingClientRect();
-      console.log([d3.event.x - bbox.x, d3.event.y - bbox.y]);
-      s.classed('active', true);
-      // console.log(d3.event.sourceEvent.target.getBoundingClientRect());
+      d3.select(this).classed('active', true);
     }
 
     function dragged(d) {
-      pos = [d3.event.x, d3.event.y];
-      const [x, y] = pos;
-      console.log(x.toFixed(4), y.toFixed(4), 'end');
+      let [x, y] = [d3.event.x, d3.event.y];
+      if (x < 0) {
+        x = 0;
+      }
+      if (y < 0) {
+        y = 0;
+      }
+      pos = [x, y];
       d3.select(this).attr('transform', `translate(${x},${y})`);
     }
 
     function dragended(d) {
       pos = pos.map(v => Math.round(v));
       const [x, y] = pos;
-      d3.select(this)
-        .attr('transform', `translate(${x},${y})`)
-        .classed('active', false);
+      const control = elements.controls.find(_control => _control.get('_id').value === this.id);
+      control.patchValue({ x, y });
+      d3.select(this).classed('active', false);
+    }
+
+    function zoomed() {
+      svg.attr('transform', d3.event.transform);
     }
 
     const drag = d3.drag().on('start', dragstarted).on('drag', dragged).on('end', dragended);
+    this.zoom = d3.zoom().on('zoom', zoomed);
+    this.svgSelection.call(this.zoom);
 
     this.els.changes.pipe(
       map(ql => ql.toArray()),
@@ -223,13 +265,16 @@ export class Template2Component implements AfterViewInit {
       map(([a, b]) => b.filter(el => a.indexOf(el) === -1)),
       flatMap(arr => from(arr)),
     ).subscribe(el => {
-      d3.select(el).call(drag);
+      const s = d3.select(el);
+      s.call(drag);
     });
   }
 
   addElement() {
     this.elements.push(this.fb.group({
       _id: uuid(),
+      x: [0],
+      y: [0],
       text: ['New Element', Validators.required],
       color: ['#FFFFFF'],
       font: ['font9x18'],
